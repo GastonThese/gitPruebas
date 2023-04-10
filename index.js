c4a2a28c30830d1472bf48c89bd3e8cd4a7b4d04
Cecilia Gomez <cecilia.gomez@these.com.uy>
2023-04-10 10:27:55 -0300

    prueba-2-cecibash: line 1: WQ: command not found

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const queDeboHacer = (tarea) => {
-  const tareas = { comprar: "comprar leche y pan" };
+  const tareas = { comprar: "comprar leche y pan", pagar: "ute" };
   console.log(tareas[tarea]);
 };
